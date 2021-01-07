5cf65e24d5ff9494f94ead2b3527fd27145c6e23
Rob Brunner <rob.brunner1@gmail.com>
2021-01-06 18:09:18 -0600

    LineGraph skeleton

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Line } from "react-chartjs-2";
 
 function LineGraph() {
+    const [data, setData] = useState({});
+
+    //fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+
+    useEffect (() => {
+      fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+            // TODO
+        })
+    }, [])
+
     return (
         <div>
-            
+          <Line
+          data
+          options
+          />
         </div>
     )
 }
