eee12d9658a1aa5c27e53da4f5f95ea805aa1745
Rob Brunner <rob.brunner1@gmail.com>
2021-01-08 16:13:55 -0600

    linegraph updates when infoBox onClick

--- src/App.js
+++ src/App.js
@@ -115,8 +115,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
-          <h3>Worldwide New Cases</h3>
-          <LineGraph />
+          <h3>Worldwide new {casesType}</h3>
+          <LineGraph casesType={casesType} />
         </CardContent>
       </Card>        
     </div>
