80344b12474fb111937c4a66624f95743ccbdfe3
Rob Brunner <rob.brunner1@gmail.com>
2021-01-08 16:56:45 -0600

    recovered infoBox font green

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -18,7 +18,7 @@ function InfoBox({ title, cases, isRed, active, total, ...props }) {
           {/* Title */}
           <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
           {/* Number of cases */}
-          <h2 className="infoBox__cases">{cases}</h2>
+          <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
           {/* Total cases */}
           <Typography className="infoBox__total" color="textSecondary">{total} Total</Typography>
       </CardContent>
