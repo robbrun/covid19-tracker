b4dc7f515c2a17e3a362ce62053b1c16e64f183a
Rob Brunner <rob.brunner1@gmail.com>
2021-01-07 18:32:01 -0600

    map working

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import "./App.css";
 import Table from "./Table";
 import { sortData } from "./util";
 import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
 
 function App() {
     const [countries, setCountries] = useState([]);
     const [country, setCountry] = useState(['worldwide']);
     const [countryInfo, setCountryInfo] = useState({});
     const [tableData, setTableData] = useState([]);
+    const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+    const [mapZoom, setMapZoom] = useState([3]);
 
     useEffect(() => {
       fetch("https://disease.sh/v3/covid-19/all")
@@ -49,9 +52,10 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
           setCountry(countryCode);
-
           // All of the data from the country response
           setCountryInfo(data);
+          setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+          setMapZoom(4);
       });
     };
 
@@ -79,15 +83,13 @@ function App() {
             <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />   
             
         </div>
-          <Map />
+          <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          {/* Table */}
           <Table countries={tableData} />
           <h3>Worldwide New Cases</h3>
-          {/* Graph */}
           <LineGraph />
         </CardContent>
       </Card>        
--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
 
-function Map() {
+function Map({ center, zoom }) {
     return (
-        <div className="map">
-        <h1>I am a map</h1>
-        </div>
-    )
+      <div className="map">
+        <LeafletMap center={center} zoom={zoom}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+        </LeafletMap>
+      </div>
+  );
 }
 
-export default Map
+export default Map;
